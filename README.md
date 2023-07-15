dc2707a3d40e5cc91caf235516c0253a7afd6262
Nibaldo Pino A <npino@uc.cl>
2023-07-15 13:03:34 +0200

    Changes in directories and github repository

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# nlp_project_mia3
+# Proyecto NLP mIA^3
+
 Project created for the Natural Language Processing (NLP) module of the Master in Applied Artificial Intelligence of the Universitat de València.
+
+---
+Apertura: sábado, 20 de mayo de 2023, 00:00
+Cierre: domingo, 16 de julio de 2023, 00:00
+
+## Objetivos
+
+Se trata de un problema de clasificación multietiqueta en el que tenéis que etiquetar un conjunto de tweets de acuerdo a 11 sentimientos ('anger', 'anticipation', 'disgust', 'fear', 'joy', 'love', 'optimism', 'pessimism', 'sadness', 'surprise', 'trust').
+Disponéis de un conjunto de 3561 tweets etiquetas para entrenar y validar el modelo o modelos necesarios, en el archivo 'sem_eval_train_es.csv'.
+
+## Requerimientos
+
+Cada alumno tiene que validar sus modelos sobre el conjunto ciego de test con el nombre 'sem_eval_test_blank_es.csv'. Cada Tweet tiene un ID (primera columna) que tenéis que mantener en vuestra solución. Debéis aplicar vuestro modelo entrenado a este conjunto y guardarlo en un archivo CSV de nombre 'soluciones_nombre_apellido1_apellido2.csv'. El archivo CSV a entregar debe tener esta estructura de Pandas:
+
+´´´python
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 679 entries, 0 to 678
+Data columns (total 12 columns):
+ #   Column        Non-Null Count  Dtype 
+---  ------        --------------  ----- 
+ 0   ID            679 non-null    object
+ 1   anger         679 non-null    bool  
+ 2   anticipation  679 non-null    bool  
+ 3   disgust       679 non-null    bool  
+ 4   fear          679 non-null    bool  
+ 5   joy           679 non-null    bool  
+ 6   love          679 non-null    bool  
+ 7   optimism      679 non-null    bool  
+ 8   pessimism     679 non-null    bool  
+ 9   sadness       679 non-null    bool  
+ 10  surprise      679 non-null    bool  
+ 11  trust         679 non-null    bool  
+dtypes: bool(11), object(1)
+´´´
+
+Si el archivo no tiene exactamente esa estructura no se podrá evaluar.
+
+## Entregable
+
+Cada alumno deberá entregar a través de esta tarea habilitada en AV el archivo CSV de las predicciones en el conjunto ciego de test y todo el código en Python utilizado tanto para el entrenamiento de los modelos como la inferencia en el conjunto ciego de test. El código Python se entregará como Notebook de Jupyter.
---
+++ utils/functions.py
@@ -0,0 +1,120 @@
+import re
+import emoji
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn import BCEWithLogitsLoss
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.nn.functional import sigmoid
+
+from transformers import XLMRobertaTokenizer, XLMRobertaForSequenceClassification, AdamW
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification,DataCollatorWithPadding
+from datasets import load_dataset, concatenate_datasets, DatasetDict
+
+
+# Limpieza de tweet
+def preprocess_tweet(tweet):
+    # Elimina los espacios en blanco al inicio y al final
+    tweet = tweet.strip()
+    # Elimina las menciones
+    tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
+    # Mantiene las palabras de los hashtags pero elimina el carácter hashtag
+    tweet = re.sub(r'#(\w+)', r'\1', tweet)
+    # Convierte los emojis a texto
+    tweet = emoji.demojize(tweet)
+    # Elimina caracteres especiales, excepto los dos puntos
+    tweet = re.sub(r'[^\w\sáéíóúÁÉÍÓÚÑñ:]', '', tweet)
+    # Convierte el tweet a minúsculas
+    tweet = tweet.lower()
+    # Elimina los espacios en blanco al inicio y al final
+    tweet = tweet.strip()
+    return tweet
+
+
+# Ajuste del tokenizador
+def process(df):
+    tokenized_inputs = tokenizer(
+       df["Tweet"], padding="max_length", truncation=True
+    )
+    return tokenized_inputs
+
+
+# Crea un DataLoader
+def create_data_loader(df, tokenizer, max_len, batch_size):
+    ds = EmotionDataset(
+        tweets=df.Tweet.to_numpy(),
+        labels=df[emotions].to_numpy(),
+        tokenizer=tokenizer,
+        max_len=max_len
+    )
+
+    return DataLoader(
+        ds,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+
+# Evalua el modelo
+def evaluate_model(model, data_loader):
+    model = model.eval()
+
+    final_loss = 0
+    counter = 0
+
+    with torch.no_grad():
+        for d in data_loader:
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            labels = d["labels"].to(device)
+
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                labels=labels
+            )
+
+            loss = outputs.loss
+            final_loss += loss.item()
+            counter += 1
+
+    return final_loss / counter
+
+
+# obtener predicciones
+def get_predictions(model, data_loader):
+    model = model.eval()
+    predictions = []
+    real_values = []
+
+    with torch.no_grad():
+        for d in data_loader:
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            labels = d["labels"].to(device)
+
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+            preds = torch.sigmoid(outputs.logits)
+            preds = preds.cpu().detach().numpy()
+            labels = labels.cpu().detach().numpy()
+
+            predictions.extend(preds)
+            real_values.extend(labels)
+
+    predictions = np.array(predictions)
+    real_values = np.array(real_values)
+    return predictions, real_values
